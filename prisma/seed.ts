9d537db124cbb9f5cc2f49cfb81ac40b7c26869d
Filip Makara <makara.filip@gmail.com>
2023-09-28 12:14:11 +0200

    Structure core database schemas

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,16 +2,91 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-async function main() {
-  const post = await prisma.post.create({
-    data: {
-      title: "First Post",
-      content:
-        "This is my first post. I hope I love writing blogs as much as I love writing them.",
+async function seedIngredients() {
+  await prisma.ingredient.upsert({
+    where: { id: 1 },
+    update: {},
+    create: {
+      id: 1,
+      name: "egg",
+      unitType: "COUNT",
+    },
+  });
+  await prisma.ingredient.upsert({
+    where: { id: 2 },
+    update: {},
+    create: {
+      id: 2,
+      name: "chicken breast",
+      unitType: "WEIGHT",
+    },
+  });
+}
+
+async function seedRecipes() {
+  await prisma.recipe.upsert({
+    where: { id: 1 },
+    update: {},
+    create: {
+      id: 1,
+      name: "Scrambled Eggs",
+      description: "A simple recipe for scrambled eggs.",
+      directions: [
+        "Crack the eggs into a bowl.",
+        "Whisk the eggs with a fork until single consistency.",
+        "Add salt and pepper to taste.",
+        "Heat a pan on medium heat. Add butter to the pan and let it melt.",
+        "Pour the eggs into the pan. Stir the eggs until cooked.",
+        "Remove from heat.",
+      ],
+      equipment: ["PAN"],
+      totalTime: 5,
+      mealType: "BREAKFAST",
+      servings: 1,
+    },
+  });
+  await prisma.recipe.upsert({
+    where: { id: 2 },
+    update: {},
+    create: {
+      id: 2,
+      name: "Beef Steak with Potatoes and Broccoli",
+      description:
+        "Simpler than it sounds. I hereby test a long text which should be wrapped and, of course, multiple lines. Maybe 2 or so. On mobile screens, even more.",
+      directions: [
+        "Preheat oven to 200 degrees Celsius.",
+        "Cut potatoes into wedges.",
+        "Place potatoes on a baking tray and drizzle with olive oil. Season with salt and pepper.",
+        "Bake potatoes for 30 minutes.",
+        "Season steak with salt and pepper.",
+        "Heat a pan on high heat. Add butter to the pan and let it melt.",
+        "Add steak to the pan and cook for 3 minutes on each side.",
+        "Remove steak from pan and let it rest.",
+        "Add broccoli to the pan and cook for 5 minutes.",
+        "Remove broccoli from pan.",
+        "Slice steak.",
+        "Serve steak with potatoes and broccoli.",
+      ],
+      equipment: ["PAN", "OVEN"],
+      totalTime: 50,
+      mealType: "MAIN",
+      servings: 3,
     },
   });
 
-  console.log({ post });
+  await prisma.recipeIngredient.create({
+    data: {
+      recipeId: 2,
+      ingredientId: 2,
+      amount: 2,
+      unit: "GRAM",
+    },
+  });
+}
+
+async function main() {
+  await seedIngredients();
+  await seedRecipes();
 }
 
 main()
