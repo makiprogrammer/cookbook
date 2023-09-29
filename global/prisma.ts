252bd8d7d596a8a349d2c3d6703212125227e7d6
Filip Makara <makara.filip@gmail.com>
2023-09-29 09:46:44 +0200

    Fix Prisma client on development server

--- global/prisma.ts
+++ global/prisma.ts
@@ -1,3 +1,16 @@
 import { PrismaClient } from "@prisma/client";
 
-export const prisma = new PrismaClient();
+// Implemented according to the Prisma NextJS best practices:
+// https://www.prisma.io/docs/guides/other/troubleshooting-orm/help-articles/nextjs-prisma-client-dev-practices
+
+const createClient = () => new PrismaClient();
+
+type PrismaClientSingleton = ReturnType<typeof createClient>;
+
+const globalForPrisma = globalThis as {
+  prisma?: PrismaClientSingleton;
+};
+
+globalForPrisma.prisma = globalForPrisma.prisma ?? createClient();
+
+export const prisma = globalForPrisma.prisma;
